d492ebca22dfd829cad09595175b260298a89186
github username <github email address>
2019-07-29 08:20:11 -0400

    built add video form

--- src/Components/AddVideoForm.js
+++ src/Components/AddVideoForm.js
@@ -1,7 +1,17 @@
 import React from 'react'
 import { Form } from 'semantic-ui-react'
+import { debounce } from 'lodash'
 
 class AddVideoForm extends React.Component {
+
+
+  state = {
+    name: "",
+    instructor: "",
+    description: "",
+    vidId: "",
+    length: "",
+  }
 
   options = [
     { key: "Mod 1", text: "Mod 1", value: "mod 1"},
@@ -11,20 +21,29 @@ class AddVideoForm extends React.Component {
     { key: "Mod 5", text: "Mod 5", value: "mod 5"},
   ]
 
+  handleChange = (e,s) => {
+    e.preventDefault()
+    let newState = {...this.state}
+    newState[s.name] = s.value
+    this.setState(
+      newState
+    )
+  }
+
   render(){
     return(
       <Form>
         <Form.Group widths='equal'>
-          <Form.Input fluid label="Video Name" placeholder="Video Name"/>
-          <Form.Input fluid label="Instructor" placeholder="Instructor"/>
+          <Form.Input fluid name="name" label="Video Name" placeholder="Video Name" onChange={debounce(this.handleChange, 500)}/>
+          <Form.Input fluid name="instructor" label="Instructor" placeholder="Instructor" onChange={debounce(this.handleChange, 500)}/>
         </Form.Group>
         <Form.Group widths="equal">
-          <Form.Input fluid label="Video Description" placeholder="Video Description"/>
+          <Form.Input fluid name="description" label="Video Description" placeholder="Video Description" onChange={debounce(this.handleChange, 500)}/>
         </Form.Group>
         <Form.Group widths="equal">
-          <Form.Input fluid label="Video ID" placeholder="youtube.com/watch?v=[ID]"/>
-          <Form.Input fluid label="Length" placeholder="In Minutes"/>
-          <Form.Select fluid label="Select Mod" placeholder="Select Mod" options={this.options} />
+          <Form.Input fluid name="vidId" label="Video ID" placeholder="youtube.com/watch?v=[ID]" onChange={debounce(this.handleChange, 500)}/>
+          <Form.Input fluid name="length" label="Length" placeholder="In Minutes" onChange={debounce(this.handleChange, 500)}/>
+          <Form.Select fluid name="mod" label="Select Mod" placeholder="Select Mod" options={this.options} onChange={debounce(this.handleChange, 500)}/>
         </Form.Group>
       </Form>
     )
